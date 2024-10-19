8755acc854a11cf9a11ea53d72f4389d3166e3f5
Sasmitha Kariyawasam <Sasamavikariyawasam12@gmail.com>
2024-10-19 13:47:50 +0530

    Add files via upload

---
+++ pr6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout<<"Enter first two parts of your name: "<<endl;
+    getline(cin,name);
+    cout<<"Hello "<<name;
+    return 0;
+}
